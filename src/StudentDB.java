1ca7c301f75a0db6e46bbbd941031de6be2ccf8c
imac1 <Februarie1977>
2019-10-21 15:03:29 +0300

    Create an application to manage student enrollments and balance.

---
+++ src/StudentDB.java
@@ -0,0 +1,10 @@
+public class StudentDB {
+
+    private String firstName;
+    private String lastName;
+    private String coursename;
+    private int tuitionFee;
+    private int yearOfStudy;
+    private int studentID;
+    private int balanceAccount;
+}
