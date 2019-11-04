027ad10372183d6b152671ad6164786c151101b8
imac1 <Februarie1977>
2019-11-04 13:28:03 +0200

    student and admin methods implemented

--- src/Student.java
+++ src/Student.java
@@ -1,10 +1,83 @@
+import java.util.Scanner;
+
 public class Student {
 
     private String firstName;
     private String lastName;
-    private String coursename;
+    public String courseName = "";
+    public String courses = "";
     private int tuitionFee;
-    private int yearOfStudy;
+    private int yearOfStudy = 0;
+    private int id = 1000;
+    private int balanceAccount = 0;
+    private int courseCost = 600;
     private int studentID;
-    private int balanceAccount;
+
+
+    public Student() {
+
+        Scanner scanner = new
+                Scanner(System.in);
+        System.out.println("Enter first name ");
+        this.firstName = scanner.nextLine();
+        System.out.println("Enter last name ");
+        this.lastName = scanner.nextLine();
+        System.out.println("Enter grade level:\n 1. Junior\n 2. Intermediate\n 3. Senior ");
+        this.yearOfStudy = scanner.nextInt();
+        setStudentId();
+
+        System.out.println(firstName + " " + lastName + " " + yearOfStudy + " student ID: " + studentID);
+
+        // scanner.close();
+
+    }
+
+
+    private void setStudentId() {
+        id++;
+        this.studentID = Integer.parseInt(yearOfStudy + "" + id);
+        return;
+    }
+
+    public void enroll() {
+
+
+        Scanner scanner = new
+                Scanner(System.in);
+
+        while (courseName != "q") {
+
+            System.out.println("Choose your courses : \n 1. History 101\n 2. Mathematics 101\n 3. English 101\n " +
+                    "4. Chemistry 101\n 5. Computer Science 101\n 6. q");
+
+            this.courseName = scanner.nextLine();
+            courses = courses + "\n" + courseName;
+            tuitionFee = tuitionFee + courseCost;
+            System.out.println("you chose " + courses );
+
+            if (courseName.equalsIgnoreCase("q")) {
+                System.out.println("tk");
+                System.out.println("Tuition fees " + tuitionFee);
+                break;
+            }
+
+            System.out.println("Tuition fees " + tuitionFee);
+
+        }
+
+    }
+
+    public void payTuition(){
+        Scanner scanner = new
+                Scanner(System.in);
+
+
+        System.out.println("How much do you want to pay: ");
+        int payment = scanner.nextInt();
+
+        this.balanceAccount = tuitionFee - payment;
+        System.out.println("you have " + balanceAccount + " to pay");
+
+    }
+
 }
--- src/StudentDB.java
+++ src/StudentDB.java
@@ -1,6 +1,38 @@
+import java.util.Scanner;
+
 public class StudentDB {
 
     public static void main(String[] args) {
+        Scanner scanner = new
+                Scanner(System.in);
+        int sSchoice=0;
 
+        System.out.println("Choose your option:\n 1. Admin\n 2. Student");
+        int option = scanner.nextInt();
+        Student ss = new Student();
+
+        if (option == 1) {
+            Student [] studentsNr = new Student[sSchoice];
+            System.out.println("Welcome admin");
+            System.out.println("How many students will you add?");
+            sSchoice = scanner.nextInt();
+
+
+            for (int i=0; i<sSchoice; i++) {
+                studentsNr[i] = new Student();
+                System.out.println(studentsNr[i].toString());
+            }
+
+            } else if (option == 2) {
+            System.out.println("Welcome student");
+            ss.enroll();
+            ss.payTuition();
+        }
+
+
+
+
+
+scanner.close();
     }
 }
